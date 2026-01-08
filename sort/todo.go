4ce381eb9dee63510525225b3e553d0003f4b0be
Vitaly Smirnov <mrmaybelately@gmail.com>
2026-01-09 02:13:52 +0300

    add sort/todo.go

---
+++ sort/todo.go
@@ -0,0 +1,9 @@
+package main
+
+import "cmp"
+
+func CountingSort[T cmp.Ordered](arr []T)  {}
+func RadixSort[T cmp.Ordered](arr []T)     {}
+func SelectionSort[T cmp.Ordered](arr []T) {}
+func InsertionSort[T cmp.Ordered](arr []T) {}
+func BubbleSort[T cmp.Ordered](arr []T)    {}
