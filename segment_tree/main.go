5812690bc1692e8bb1d62328dca609ff95221f77
Vitaly Smirnov <mrmaybelately@gmail.com>
2026-02-11 11:05:55 +0300

    update segment_tree

--- segment_tree/main.go
+++ segment_tree/main.go
@@ -3,79 +3,9 @@ package main
 import (
 	"fmt"
 	"math/rand/v2"
+	"slices"
+	"time"
 )
-
-type SegmentTree struct{ sums []int }
-
-func MakeSegmentTree(arr []int) *SegmentTree {
-	st := &SegmentTree{sums: make([]int, len(arr)*4)}
-	st.build(arr, 0, 0, len(arr)-1)
-	return st
-}
-
-func (st *SegmentTree) build(arr []int, pos int, segLeft, segRight int) {
-	if segLeft == segRight {
-		st.sums[pos] = arr[segLeft]
-	} else {
-		mid := segLeft + (segRight-segLeft)/2
-		leftChild, rightChild := pos*2+1, pos*2+2
-		st.build(arr, leftChild, segLeft, mid)
-		st.build(arr, rightChild, mid+1, segRight)
-		st.sums[pos] = st.sums[leftChild] + st.sums[rightChild]
-	}
-}
-
-func (st *SegmentTree) Sum(left, right int) int {
-	return st.sum(0, 0, len(st.sums)/4-1, left, right)
-}
-
-func (st *SegmentTree) sum(pos int, segLeft, segRight, qLeft, qRight int) int {
-	if qLeft > qRight {
-		return 0
-	}
-	if segLeft == qLeft && segRight == qRight {
-		return st.sums[pos]
-	}
-	mid := segLeft + (segRight-segLeft)/2
-	return st.sum(pos*2+1, segLeft, mid, qLeft, min(qRight, mid)) +
-		st.sum(pos*2+2, mid+1, segRight, max(qLeft, mid+1), qRight)
-}
-
-func (st *SegmentTree) Update(index int, value int) {
-	st.update(0, 0, len(st.sums)/4-1, index, value)
-}
-
-func (st *SegmentTree) update(pos int, segLeft, segRight int, index int, value int) {
-	if segLeft == segRight {
-		st.sums[pos] = value
-	} else {
-		mid := segLeft + (segRight-segLeft)/2
-		if index <= mid {
-			st.update(pos*2+1, segLeft, mid, index, value)
-		} else {
-			st.update(pos*2+2, mid+1, segRight, index, value)
-		}
-		st.sums[pos] = st.sums[pos*2+1] + st.sums[pos*2+2]
-	}
-}
-
-func (st *SegmentTree) Nums() []int {
-	res := make([]int, len(st.sums)/4)
-	for i := range res {
-		res[i] = st.Sum(i, i)
-	}
-	return res
-}
-
-// temp
-
-func sum(arr []int, left, right int) int {
-	sum := 0
-	for ; left <= right; left++ {
-		sum += arr[left]
-	}
-	return sum
-}
 
 func GenerateNums(size int, minNum, maxNum int) []int {
 	nums := make([]int, size)
@@ -84,6 +14,14 @@ func GenerateNums(size int, minNum, maxNum int) []int {
 		nums[i] = rand.IntN(numsRange) + minNum
 	}
 	return nums
+}
+
+func sum(arr []int, left, right int) int {
+	sum := 0
+	for ; left <= right; left++ {
+		sum += arr[left]
+	}
+	return sum
 }
 
 type STTest struct {
@@ -110,7 +48,7 @@ func (t *STTest) Update(index int, value int) {
 	t.st.Update(index, value)
 }
 
-func (t *STTest) Update2() { //index int, value int) {
+func (t *STTest) Update2() {
 	index := rand.IntN(len(t.nums))
 	value := rand.IntN(t.maxNum-t.minNum+1) + t.minNum
 	t.nums[index] = value
@@ -120,27 +58,18 @@ func (t *STTest) Update2() { //index int, value int) {
 // todo: cleanup
 
 func testSegmentTree() {
-	// nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// n := len(nums)
-	// st := MakeSegmentTree(nums)
-	// fmt.Println(nums)
-	// fmt.Println(st.Sum(0, n-1))
-	// fmt.Println(sum(nums, 0, n-1))
-	// st.
-
-	// nums2 := Nums{}
-	// nums2.Generate(10, 0, 9)
-	// fmt.Println(nums2.nums)
-
 	const numsLen = 1_000_000
 	const minNum = -1_000_000
 	const maxNum = 1_000_000
 
+	t := time.Now()
 	test := MakeSTTest(numsLen, minNum, maxNum)
-	for range 1000 {
+	if !slices.Equal(test.nums, test.st.Nums()) {
+		fmt.Println("nums and ST are not equal")
+	}
+	for range 10000 {
 		left := rand.IntN(numsLen)
 		right := left + rand.IntN(numsLen-left)
-		// fmt.Println(left, right)
 		if !test.TestSum(left, right) {
 			fmt.Println("sums are not equal")
 		}
@@ -152,14 +81,9 @@ func testSegmentTree() {
 			fmt.Println("sums are not equal after update")
 		}
 	}
+	fmt.Printf("time: %v\n", time.Since(t))
 }
 
 func main() {
 	testSegmentTree()
-
-	// nums := GenerateNums(6, 1, 9)
-	// st := MakeSegmentTree(nums)
-	// fmt.Println(nums)
-	// fmt.Println(st.Nums())
-	// fmt.Println(st.sums)
 }
---
+++ segment_tree/sum_segment_tree.go
@@ -0,0 +1,63 @@
+package main
+
+type SegmentTree struct{ sums []int }
+
+func MakeSegmentTree(arr []int) *SegmentTree {
+	st := &SegmentTree{sums: make([]int, len(arr)*4)}
+	st.build(arr, 0, 0, len(arr)-1)
+	return st
+}
+
+func (st *SegmentTree) build(arr []int, pos int, segLeft, segRight int) {
+	if segLeft == segRight {
+		st.sums[pos] = arr[segLeft]
+	} else {
+		mid := segLeft + (segRight-segLeft)/2
+		leftChild, rightChild := pos*2+1, pos*2+2
+		st.build(arr, leftChild, segLeft, mid)
+		st.build(arr, rightChild, mid+1, segRight)
+		st.sums[pos] = st.sums[leftChild] + st.sums[rightChild]
+	}
+}
+
+func (st *SegmentTree) Sum(left, right int) int {
+	return st.sum(0, 0, len(st.sums)/4-1, left, right)
+}
+
+func (st *SegmentTree) sum(pos int, segLeft, segRight, qLeft, qRight int) int {
+	if qLeft > qRight {
+		return 0
+	}
+	if segLeft == qLeft && segRight == qRight {
+		return st.sums[pos]
+	}
+	mid := segLeft + (segRight-segLeft)/2
+	return st.sum(pos*2+1, segLeft, mid, qLeft, min(qRight, mid)) +
+		st.sum(pos*2+2, mid+1, segRight, max(qLeft, mid+1), qRight)
+}
+
+func (st *SegmentTree) Update(index int, value int) {
+	st.update(0, 0, len(st.sums)/4-1, index, value)
+}
+
+func (st *SegmentTree) update(pos int, segLeft, segRight int, index int, value int) {
+	if segLeft == segRight {
+		st.sums[pos] = value
+	} else {
+		mid := segLeft + (segRight-segLeft)/2
+		if index <= mid {
+			st.update(pos*2+1, segLeft, mid, index, value)
+		} else {
+			st.update(pos*2+2, mid+1, segRight, index, value)
+		}
+		st.sums[pos] = st.sums[pos*2+1] + st.sums[pos*2+2]
+	}
+}
+
+func (st *SegmentTree) Nums() []int {
+	res := make([]int, len(st.sums)/4)
+	for i := range res {
+		res[i] = st.Sum(i, i)
+	}
+	return res
+}
