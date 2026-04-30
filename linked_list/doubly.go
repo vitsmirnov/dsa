a110601b75a20c4cb4f26f211861dec1435eb37e
Vitaly Smirnov <mrmaybelately@gmail.com>
2026-05-01 00:55:39 +0300

    update linked_list (add merge sort)

--- linked_list/doubly.go
+++ linked_list/doubly.go
@@ -1,8 +1,8 @@
 package main
 
 type LinkedListNode[T any] struct {
-	data       T
-	prev, next *LinkedListNode[T]
+	Data       T
+	Prev, Next *LinkedListNode[T]
 }
 
 type LinkedList[T any] struct {
@@ -10,11 +10,13 @@ type LinkedList[T any] struct {
 	length      int
 }
 
-func (l *LinkedList[T]) Len() int { return l.length }
+func (l *LinkedList[T]) Len() int                  { return l.length }
+func (l *LinkedList[T]) Front() *LinkedListNode[T] { return l.front }
+func (l *LinkedList[T]) Back() *LinkedListNode[T]  { return l.back }
 
 func (l *LinkedList[T]) pushBack(node *LinkedListNode[T]) *LinkedListNode[T] {
 	if l.back != nil {
-		l.back.next = node
+		l.back.Next = node
 	} else {
 		l.front = node
 	}
@@ -24,30 +26,30 @@ func (l *LinkedList[T]) pushBack(node *LinkedListNode[T]) *LinkedListNode[T] {
 }
 
 func (l *LinkedList[T]) PushBack(value T) *LinkedListNode[T] {
-	return l.pushBack(&LinkedListNode[T]{data: value, prev: l.back, next: nil})
+	return l.pushBack(&LinkedListNode[T]{Data: value, Prev: l.back, Next: nil})
 }
 
 func (l *LinkedList[T]) PopFront() T { return l.Remove(l.front) }
 
 func (l *LinkedList[T]) Remove(node *LinkedListNode[T]) T {
-	if node.prev != nil {
-		node.prev.next = node.next
+	if node.Prev != nil {
+		node.Prev.Next = node.Next
 	} else {
-		l.front = node.next
+		l.front = node.Next
 	}
-	if node.next != nil {
-		node.next.prev = node.prev
+	if node.Next != nil {
+		node.Next.Prev = node.Prev
 	} else {
-		l.back = node.prev
+		l.back = node.Prev
 	}
 	l.length--
-	return node.data
+	return node.Data
 }
 
 func (l *LinkedList[T]) MoveToBack(node *LinkedListNode[T]) {
 	l.Remove(node)
-	node.prev = l.back
-	node.next = nil
+	node.Prev = l.back
+	node.Next = nil
 	l.pushBack(node)
 }
 
--- linked_list/singly.go
+++ linked_list/singly.go
@@ -1,12 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"cmp"
+	"fmt"
+)
 
 // todo: review, iterator
 
 type LinkedListNode1[T any] struct {
-	data T
-	next *LinkedListNode1[T]
+	Data T
+	Next *LinkedListNode1[T]
 }
 
 type LinkedList1[T any] struct {
@@ -22,23 +25,25 @@ func MakeLinkedList1[T any]() *LinkedList1[T] {
 		length: 0}
 }
 
-func (this *LinkedList1[T]) Len() int { return this.length }
+func (this *LinkedList1[T]) Len() int                  { return this.length }
+func (this *LinkedList1[T]) Head() *LinkedListNode1[T] { return this.head }
+func (this *LinkedList1[T]) Tail() *LinkedListNode1[T] { return this.tail }
 
 func (this *LinkedList1[T]) Get(index int) T {
 	if !(0 <= index && index < this.length) {
 		panic(fmt.Sprintf("LinkedList1: index (%v) out of range [0-%v)", index, this.length))
 	}
 	if index == 0 {
-		return this.head.data
+		return this.head.Data
 	} else if index == this.length-1 {
-		return this.tail.data
+		return this.tail.Data
 	} else {
-		return this.getNode(index).data
+		return this.getNode(index).Data
 	}
 }
 
 func (this *LinkedList1[T]) AddAtHead(data T) {
-	this.head = &LinkedListNode1[T]{data: data, next: this.head}
+	this.head = &LinkedListNode1[T]{Data: data, Next: this.head}
 	if this.tail == nil {
 		this.tail = this.head
 	}
@@ -47,11 +52,11 @@ func (this *LinkedList1[T]) AddAtHead(data T) {
 
 func (this *LinkedList1[T]) AddAtTail(data T) {
 	if this.tail == nil {
-		this.tail = &LinkedListNode1[T]{data: data, next: nil}
+		this.tail = &LinkedListNode1[T]{Data: data, Next: nil}
 		this.head = this.tail
 	} else {
-		this.tail.next = &LinkedListNode1[T]{data: data, next: nil}
-		this.tail = this.tail.next
+		this.tail.Next = &LinkedListNode1[T]{Data: data, Next: nil}
+		this.tail = this.tail.Next
 	}
 	this.length++
 }
@@ -66,7 +71,7 @@ func (this *LinkedList1[T]) AddAtIndex(index int, data T) {
 		this.AddAtTail(data)
 	} else {
 		prev := this.getNode(index - 1)
-		prev.next = &LinkedListNode1[T]{data: data, next: prev.next}
+		prev.Next = &LinkedListNode1[T]{Data: data, Next: prev.Next}
 		this.length++
 	}
 }
@@ -79,10 +84,10 @@ func (this *LinkedList1[T]) DeleteAtIndex(index int) {
 		if this.tail == this.head {
 			this.tail = nil
 		}
-		this.head = this.head.next
+		this.head = this.head.Next
 	} else {
 		prev := this.getNode(index - 1)
-		prev.next = prev.next.next
+		prev.Next = prev.Next.Next
 		if index == this.length-1 {
 			this.tail = prev
 		}
@@ -96,7 +101,62 @@ func (this *LinkedList1[T]) getNode(index int) *LinkedListNode1[T] {
 	}
 	cur := this.head
 	for range index {
-		cur = cur.next
+		cur = cur.Next
 	}
 	return cur
+}
+
+// todo: tests for sorting
+// change to interface?
+
+func SortList[T cmp.Ordered](head *LinkedListNode1[T]) *LinkedListNode1[T] {
+	// time: O(n log n), space: O(log n)
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	mid := CutListInHalf(head)
+	head = SortList(head)
+	mid = SortList(mid)
+	return mergeSortedLists(head, mid)
+}
+
+func mergeSortedLists[T cmp.Ordered](list1, list2 *LinkedListNode1[T]) *LinkedListNode1[T] {
+	// time: O(n), space: O(1)
+
+	preHead := &LinkedListNode1[T]{Next: nil}
+	cur := preHead
+	for ; list1 != nil && list2 != nil; cur = cur.Next {
+		if list1.Data < list2.Data {
+			cur.Next = list1
+			list1 = list1.Next
+		} else {
+			cur.Next = list2
+			list2 = list2.Next
+		}
+	}
+	if list1 != nil {
+		cur.Next = list1
+	} else if list2 != nil {
+		cur.Next = list2
+	}
+	return preHead.Next
+}
+
+func CutListInHalf[T any](head *LinkedListNode1[T]) *LinkedListNode1[T] {
+	// time: O(n), space: O(1)
+
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	slow, fast := head, head
+	predSlow := (*LinkedListNode1[T])(nil)
+	for fast != nil && fast.Next != nil {
+		predSlow = slow
+		slow, fast = slow.Next, fast.Next.Next
+	}
+	predSlow.Next = nil
+	return slow
 }
