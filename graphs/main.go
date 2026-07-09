d6ef92d97fb17a8292fb5ac4c828c9bfde15be68
Vitaly Smirnov <mrmaybelately@gmail.com>
2026-07-09 12:00:35 +0300

    add graphs (todo)

---
+++ graphs/main.go
@@ -0,0 +1,22 @@
+package main
+
+// todo:
+// DFS
+// BFS
+// 0-1 BFS
+// Dijkstra
+// Bellman-Ford
+// Floyd-Warshall
+// MST
+//  Kraskal
+//  Prime
+// bridges (Tarjan)
+// connected components
+// Euler's path?
+// binary lifting
+// clone
+// build graph (from edges)
+
+func main() {
+
+}
